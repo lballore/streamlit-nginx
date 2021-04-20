1947520716050119246e58b167be436a42fb0cc6
Luca Ballore <luca.ballore@gmail.com>
2021-04-20 20:59:56 +0200

    feature: add dockerfiles for python 3.6, 3.7, 3.8 and 3.9 and add nginx config file

---
+++ bin/start-nginx.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euo pipefail
+
+readonly NGINX_CONFIG=/home/streamlitapp/.nginx/nginx.conf
+readonly NGINX_P="/tmp/nginx.pid"
+
+echo "$@"
+nginx -c "$NGINX_CONFIG" -p "$NGINX_P" -g "daemon off;" & $@
